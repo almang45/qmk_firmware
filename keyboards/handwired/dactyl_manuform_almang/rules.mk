d12310f0b878f457353734861dea0b5e74f69d60
Alfonsus Manggala N Subiakto <alfonsus.manggala@gdn-commerce.com>
2025-07-16 00:02:38 +0700

    fix keyboard.json and config.h

--- keyboards/handwired/dactyl_manuform_almang/rules.mk
+++ keyboards/handwired/dactyl_manuform_almang/rules.mk
@@ -1,5 +1,19 @@
-# Build options
-ENCODER_ENABLE = yes
-OLED_ENABLE = yes
-RGBLIGHT_ENABLE = yes
-SPLIT_KEYBOARD = yes
+# Minimal rules.mk for Dactyl Manuform Pi Pico
+# Most configuration moved to keyboard.json
+
+# Only include non-conflicting build rules here
+# All features are now defined in keyboard.json to avoid conflicts
+
+# Optional: Custom build optimizations
+LTO_ENABLE = yes
+
+# Optional: Reduce firmware size
+CONSOLE_ENABLE = no
+COMMAND_ENABLE = no
+
+# Note: The following features are now configured in keyboard.json:
+# - SPLIT_KEYBOARD (conflicts resolved)
+# - ENCODER_ENABLE (conflicts resolved)
+# - OLED_ENABLE (conflicts resolved)
+# - RGB_MATRIX_ENABLE (conflicts resolved)
+# - All other features moved to JSON
