4e7f62e0327a61c481fca38fda7d756502edddac
Alfonsus Manggala N Subiakto <alfonsus.manggala@gdn-commerce.com>
2025-08-06 22:14:12 +0700

    Update dactyl_manuform_pi_pico_5x6_right: encoder config, RGB lighting, and keymap improvements

--- keyboards/handwired/dactyl_manuform_pi_pico_5x6_right/keymaps/default/keymap.c
+++ keyboards/handwired/dactyl_manuform_pi_pico_5x6_right/keymaps/default/keymap.c
@@ -5,6 +5,11 @@ SPDX-License-Identifier: GPL-2.0-or-later
 
 #include QMK_KEYBOARD_H
 
+enum layer_names {
+    _BASE,
+    _FN,
+};
+
 const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
     /*
      * ┌───┬───┬───┬───┬───┬───┐
@@ -21,31 +26,40 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
      * │Ent├───┘
      * └───┘
      */
-    [0] = LAYOUT(
-        KC_PMNS, KC_PPLS, KC_PSLS, KC_PAST, KC_DEL,  KC_BSPC,
-        KC_P7,   KC_P8,   KC_P9,   UG_NEXT, KC_HOME, KC_PGUP,
-        KC_P4,   KC_P5,   KC_P6,   KC_PCMM, KC_END,  KC_PGDN,
-        KC_P1,   KC_P2,   KC_P3,   KC_PDOT, KC_VOLD, KC_VOLU,
-        KC_PENT, KC_NO,   KC_P0,   KC_PEQL
+    [_BASE] = LAYOUT(
+        KC_PMNS,   KC_PPLS,   KC_PSLS,   KC_PAST,   KC_DEL,    KC_BSPC,
+        KC_P7,     KC_P8,     KC_P9,     UG_NEXT,   KC_HOME,   KC_PGUP,
+        KC_P4,     KC_P5,     KC_P6,     KC_PCMM,   KC_END,    KC_PGDN,
+        KC_P1,     KC_P2,     KC_P3,     KC_PDOT,   KC_VOLD,   KC_VOLU,
+        KC_PENT,   MO(_FN),   KC_P0,     KC_PEQL
+    ),
+    [_FN] = LAYOUT(
+        KC_ESC,    _______,   _______,   _______,   _______,   KC_DEL,
+        _______,   _______,   KC_UP,     UG_PREV,   UG_TOGG,   _______,
+        _______,   KC_LEFT,   KC_DOWN,   KC_RIGHT,  _______,   _______,
+        _______,   _______,   _______,   _______,   _______,   _______,
+        KC_SPC,    _______,   _______,   _______
     )
 };
 
-#ifdef ENCODER_ENABLE
-#include "encoder.h"
-bool encoder_update_user(uint8_t index, bool clockwise) {
-  if (index == 0) { /* First encoder */
-    if (clockwise) {
-      tap_code(KC_VOLU);
-    } else {
-      tap_code(KC_VOLD);
-    }
-  } else if (index == 1) {
-    if (clockwise) {
-        tap_code(MS_WHLU);
-    } else {
-        tap_code(MS_WHLD);
-    }
-  }
-  return true;
+#if defined(ENCODER_MAP_ENABLE)
+const uint16_t PROGMEM encoder_map[][NUM_ENCODERS][NUM_DIRECTIONS] = {
+    [_BASE] = { ENCODER_CCW_CW(MS_WHLU, MS_WHLD),  ENCODER_CCW_CW(KC_VOLD, KC_VOLU)  },
+    [_FN]   = { ENCODER_CCW_CW(KC_PGDN, KC_PGUP),  ENCODER_CCW_CW(UG_SPDU, UG_SPDD)  }
+};
+#endif
+
+#ifdef OLED_ENABLE
+bool oled_task_user(void) {
+    // Host Keyboard Layer Status
+    oled_write_P(PSTR("Layer: Default\n"), false);
+
+    // Host Keyboard LED Status
+    led_t led_state = host_keyboard_led_state();
+    oled_write_P(led_state.num_lock ? PSTR("NUM ") : PSTR("    "), false);
+    oled_write_P(led_state.caps_lock ? PSTR("CAP ") : PSTR("    "), false);
+    oled_write_P(led_state.scroll_lock ? PSTR("SCR ") : PSTR("    "), false);
+
+    return false;
 }
 #endif
