972f4b521e7b3bb20ee7a0bc6413dce2b7cd7537
Alfonsus Manggala N Subiakto <alfonsus.manggala@gdn-commerce.com>
2025-08-13 16:01:45 +0700

    Update dactyl manuform pi pico 5x6 right keymap configuration

--- keyboards/handwired/dactyl_manuform_pi_pico_5x6_right/keymaps/default/keymap.c
+++ keyboards/handwired/dactyl_manuform_pi_pico_5x6_right/keymaps/default/keymap.c
@@ -50,14 +50,8 @@ const uint16_t PROGMEM encoder_map[][NUM_ENCODERS][NUM_DIRECTIONS] = {
 #endif
 
 #ifdef OLED_ENABLE
-static void render_logo(void) {
-    static const char PROGMEM qmk_logo[] = {
-        0x80, 0x81, 0x82, 0x83, 0x84, 0x85, 0x86, 0x87, 0x88, 0x89, 0x8A, 0x8B, 0x8C, 0x8D, 0x8E, 0x8F, 0x90, 0x91, 0x92, 0x93, 0x94,
-        0xA0, 0xA1, 0xA2, 0xA3, 0xA4, 0xA5, 0xA6, 0xA7, 0xA8, 0xA9, 0xAA, 0xAB, 0xAC, 0xAD, 0xAE, 0xAF, 0xB0, 0xB1, 0xB2, 0xB3, 0xB4,
-        0xC0, 0xC1, 0xC2, 0xC3, 0xC4, 0xC5, 0xC6, 0xC7, 0xC8, 0xC9, 0xCA, 0xCB, 0xCC, 0xCD, 0xCE, 0xCF, 0xD0, 0xD1, 0xD2, 0xD3, 0xD4, 0x00
-    };
-
-    oled_write_P(qmk_logo, false);
+oled_rotation_t oled_init_user(oled_rotation_t rotation) {
+    return OLED_ROTATION_90;
 }
 bool oled_task_user(void) {
     // Host Keyboard Layer Status
@@ -74,11 +68,10 @@ bool oled_task_user(void) {
     }
     // Host Keyboard LED Status
     led_t led_state = host_keyboard_led_state();
-    oled_write_P(led_state.num_lock ? PSTR("NUM ") : PSTR("    "), false);
-    oled_write_P(led_state.caps_lock ? PSTR("CAP ") : PSTR("    "), false);
-    oled_write_P(led_state.scroll_lock ? PSTR("SCR ") : PSTR("    "), false);
+    oled_write_P(led_state.num_lock ? PSTR("NUM\n") : PSTR(""), false);
+    oled_write_P(led_state.caps_lock ? PSTR("CAP\n") : PSTR(""), false);
+    oled_write_P(led_state.scroll_lock ? PSTR("SCR\n") : PSTR(""), false);
 
-    render_logo();
     return false;
 }
 #endif
